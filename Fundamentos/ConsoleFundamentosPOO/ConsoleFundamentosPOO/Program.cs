84e80952821646cf953f636bd351f46d73eaafba
Gabriel da Silva Inácio <Inaciogs03@gmail.com>
2024-07-08 14:01:09 -0300

    operadores e condicionais

--- Fundamentos/ConsoleFundamentosPOO/ConsoleFundamentosPOO/Program.cs
+++ Fundamentos/ConsoleFundamentosPOO/ConsoleFundamentosPOO/Program.cs
@@ -10,6 +10,50 @@ namespace ConsoleFundamentosPOO
     {
         static void Main(string[] args)
         {
+
+            var a = 1;
+
+            //ternario
+            bool verdadeiro = (a == 1) ? true : false;
+            //condicional if else if e else
+
+            //if (a == 1) Console.WriteLine("entrou na condição if");
+            if (a == 1)
+            {
+                Console.WriteLine("entrou na condição do if");
+                Console.WriteLine("========");
+            }
+            else if(a == 2)
+            {
+                Console.WriteLine("entrou na condição do else if");
+                Console.WriteLine("========");
+            }
+            else
+            {
+                Console.WriteLine("entrou na condição senão, else.");
+                Console.WriteLine("========");
+            }
+
+            //condicional switch
+            var opcao = 0;
+            switch (opcao)
+            {
+                case 1:
+                    Console.WriteLine("opcao 1");
+                    break;
+                case 2:
+                    Console.WriteLine("opcao 2");
+                    break;
+                case 3:
+                    Console.WriteLine("opcao 3");
+                    break;
+                default:
+                    Console.WriteLine("opção 0");
+                    break;
+            }
+
+            Console.WriteLine("Olá pessoal");
+            Console.Read();
         }
     }
 }
