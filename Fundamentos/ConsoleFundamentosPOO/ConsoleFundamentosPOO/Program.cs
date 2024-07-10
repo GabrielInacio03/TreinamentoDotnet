4674297117a9ef58e1076e15b7005e168f9aa137
Gabriel da Silva Inácio <Inaciogs03@gmail.com>
2024-07-10 20:32:46 -0300

    organizando arquivos

--- Fundamentos/ConsoleFundamentosPOO/ConsoleFundamentosPOO/Program.cs
+++ Fundamentos/ConsoleFundamentosPOO/ConsoleFundamentosPOO/Program.cs
@@ -11,46 +11,8 @@ namespace ConsoleFundamentosPOO
         static void Main(string[] args)
         {
 
-            var a = 0;
-
-            //while
-            while(a <= 5)
-            {
-                Console.WriteLine($"a: {a}");
-                a++;
-            }
-
-            //do while
-            //primeiro ele faz a operação e depois verifica os valores
-            do
-            {
-                Console.WriteLine(a);
-                a++;
-            } while (a <= 5);
-
-            //for
-            int x = 10;
-            for (int i = 0; i < 10; i++)
-            {
-                Console.WriteLine(x);
-            }
-
-            //foreach
-            char[] alfabeto = {'A', 'B', 'C', 'D', 'E' };
-            foreach (var letra in alfabeto)
-            {
-                Console.WriteLine($"Letra: {letra}");
-            }
-            Console.WriteLine("Olá pessoal");
-            Console.Read();
+            
         }
-        public int loopRecursiva(int a)
-        {
-            Console.WriteLine(a);
-            if (a < 3)
-                loopRecursiva(a++);
-
-            return a;
-        }
+        
     }
 }
