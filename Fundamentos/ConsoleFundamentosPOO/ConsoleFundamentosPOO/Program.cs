dd4a9718b551d319f23d1ed0e5fabc4628f0af16
Gabriel da Silva Inácio <Inaciogs03@gmail.com>
2024-07-13 18:13:09 -0300

    função recursiva

--- Fundamentos/ConsoleFundamentosPOO/ConsoleFundamentosPOO/Program.cs
+++ Fundamentos/ConsoleFundamentosPOO/ConsoleFundamentosPOO/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,11 +15,28 @@ namespace ConsoleFundamentosPOO
             MostrarMensagemNaTela();
 
             Console.WriteLine("Informe qual tabuada? ");
-            int tab = int.Parse(Console.ReadLine());
-            Tabuada(tab);
-
-            Console.WriteLine(calcular());
+            LerArquivos(1);
             Console.Read();
+        }
+        public static void LerArquivos(int numeroArquivo)
+        {
+            string pathUrlBase = $"C:\\Users\\Inaci\\Documents\\arquivosAulasTeste\\arq{numeroArquivo}.txt";
+            if (File.Exists(pathUrlBase))
+            {
+                using (var arquivo = File.OpenText(pathUrlBase))
+                {
+                    string linha;
+                    while ((linha = arquivo.ReadLine()) != null)
+                    {
+                        Console.WriteLine(linha);
+                    }
+                    
+                }
+            }
+
+            string pathUrlBaseProximo = $"C:\\Users\\Inaci\\Documents\\arquivosAulasTeste\\arq{numeroArquivo + 1}.txt";
+            if (File.Exists(pathUrlBaseProximo))
+                LerArquivos(numeroArquivo + 1);
         }
         public static void MostrarMensagemNaTela()
         {
