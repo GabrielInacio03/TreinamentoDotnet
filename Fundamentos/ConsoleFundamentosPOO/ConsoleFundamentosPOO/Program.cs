4ce9e46715418cd1fc3bd4e5d428c9d3e9580054
inacio <inaciogs03@gmail.com>
2024-08-17 19:50:51 -0300

    poo - classe abstrata

--- Fundamentos/ConsoleFundamentosPOO/ConsoleFundamentosPOO/Program.cs
+++ Fundamentos/ConsoleFundamentosPOO/ConsoleFundamentosPOO/Program.cs
@@ -9,6 +9,26 @@ using System.Threading.Tasks;
 
 namespace ConsoleFundamentosPOO
 {
+    public abstract class Animal 
+    {
+        public string Nome { get; set; }
+        public string Cor { get; set; }
+        public string Tamanho { get; set; }
+
+        public abstract void Latir();
+        public string Correr()
+        {
+            return "Animal esta correndo";
+        }
+    }
+    public class Cachorro : Animal
+    {
+        public override void Latir()
+        {
+            Console.WriteLine("Cachorro latindo... AUHAUHAUH");
+        }
+    }
+
     class Program
     {        
         static void Main(string[] args)
